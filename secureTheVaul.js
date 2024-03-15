c05c587c90c071965f35a4e5bf382b2cbe7fa722
Alberto Martinez <avmrbeto98@gmail.com>
2024-03-15 16:21:23 -0700

    Created and updated files

---
+++ secureTheVaul.js
@@ -0,0 +1,20 @@
+// This code initializes three variables with specific arithmetic operations to match the desired combination.
+// It then constructs a message string and uses an alert to display this information to the user.
+// Alberto Martinez Ruiz 13 MAR 2024
+
+
+// Calculate the first number subtraction
+let number1 = 50 - 40; // 10
+// Calculate the second number using addition
+let number2 = 10 + 10; // 20
+// Calculate the third number using division
+let number3 = 60 / 2; // 30
+
+// Step 2: Create Message String
+// Message to the user
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+// Step 3: Display Combination
+alert(message + number1 + " - " + number2 + " - " + number3);
+
+
